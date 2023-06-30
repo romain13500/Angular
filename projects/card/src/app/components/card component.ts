7867554f313920673f1c9f967539c75b4ed742f4
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-06-30 16:35:38 +0200

    card ok

---
+++ projects/card/src/app/components/card component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: 'card',
+    template: `
+        <article>
+            <header>{{ title }}</header>
+            <ng-content></ng-content>
+            <ng-content select="footer"></ng-content>
+        </article>
+    `
+})
+
+
+export class CardComponent {
+    @Input()
+    title = " ";
+}
