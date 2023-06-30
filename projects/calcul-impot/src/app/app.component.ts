f030baff074e8edf40cf1c332212d86a5b0b2c45
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-06-30 15:44:21 +0200

    calcul + alert

--- projects/calcul-impot/src/app/app.component.ts
+++ projects/calcul-impot/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calcul-impot';
+  message = "Votre revenu après imposition est de : "
+  revenuDeBase = 1000;
+  impot = 500;
+  
+
+  calculImpot(){
+    const impots = this.revenuDeBase + this.impot;
+    return window.alert('Votre revenu est de :' + impots + '€');
+    
+  }
 }
