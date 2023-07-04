d410b82226b24b07c6033b950809459f47a09259
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-07-04 13:31:56 +0200

    create component settings

--- projects/password-generator/src/app/app.component.ts
+++ projects/password-generator/src/app/app.component.ts
@@ -2,7 +2,34 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div class="container">
+      <h1>Générez un mot de passe ! </h1>
+      <div class="grid">
+
+        <password-display [message]="message"></password-display>
+
+        <div>
+          <label for="length">Longueur de mot de passe : {{ length }}</label>
+          <input  [(ngModel)]="length" id="length" type="range" min="10" max="50" name="length"/>
+
+          <label>
+            <input  [(ngModel)]="uppercase" role="switch" type="checkbox" name="uppercase" id="uppercase"/>
+            Contiendra des "MAJUSCULES"
+          </label>
+          <label>
+            <input  [(ngModel)]="numbers" role="switch" type="checkbox" name="numbers" id="numbers"/>
+            Contiendra des "Nombres"
+          </label>
+          <label>
+            <input  [(ngModel)]="symbols" role="switch" type="checkbox" name="symbols" id="symbols"/>
+            Contiendra des "Caractères spéciaux"
+          </label>
+          <password-controls (generate)="onClickGenerate()"></password-controls>
+        </div>
+      </div>
+    </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- projects/password-generator/src/app/app.module.ts
+++ projects/password-generator/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PasswordDisplayComponent } from './components/password-display.component';
 import { PasswordControlsComponent } from './components/password-controls.component';
+import { PasswordSettingsComponent } from './components/password-settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PasswordDisplayComponent,
-    PasswordControlsComponent
+    PasswordControlsComponent,
+    PasswordSettingsComponent
   ],
   imports: [
     BrowserModule,
