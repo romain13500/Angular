af00bdfc4075e7b8bcc8c2813d12b058d2696843
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-07-05 10:43:24 +0200

    create injection

--- projects/injection/src/app/app.component.ts
+++ projects/injection/src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Injection de dépendances</h1>
+  `,
+  styles: []
 })
 export class AppComponent {
   title = 'injection';
